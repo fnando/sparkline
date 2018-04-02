ff6939a52f44480047cebe6e41814cb567b864c0
Nando Vieira <fnando.vieira@gmail.com>
2018-04-02 12:14:18 -0300

    Upgrade webpack to v4.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 let filename;
 let plugins = [];
+let mode;
 
 if (process.env.NODE_ENV === "production") {
+  mode = "production";
   filename = "sparkline.min.js";
   plugins.push(new UglifyJSPlugin({sourceMap: true}));
 } else {
+  mode = "development";
   filename = "sparkline.js";
 }
 
@@ -13,6 +16,7 @@ module.exports = {
   entry: `${__dirname}/src/sparkline.js`,
   devtool: "source-map",
   target: "web",
+  mode: mode,
   node: {
     fs: "empty"
   },
